872ef1e1535d0da6f08f8d9ee74bf13aecab0388
User <hashemqaryouti89@gmail.com>
2023-09-01 11:15:34 +0300

    First Comment

--- src/Actors/Admin.java
+++ src/Actors/Admin.java
@@ -4,7 +4,6 @@ import Database.DatabaseConnection;
 import GraphicalUserInterfaces.AdminInterfaces.DeleteLibrarianInterface;
 
 import javax.swing.*;
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
